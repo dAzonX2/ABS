a398d9a3ea6ad4eff3adc28de3c4ca70eaa6e0b3
dAzonX2 <danilazanov@gmail.com>
2021-04-29 20:36:43 +0500

    1st

---
+++ abs/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abs
+{
+    class User
+    {
+        public string Family { get; set; }
+        public string F_Name { get; set; }
+        public string S_Name { get; set; }
+        public int ID { get; set; }
+    }
+
+    class Librarian : User 
+    {
+        public string Password { get; set; }
+    }
+    class Reader : User
+    {
+        public string LibCardDate { get; set; }
+        public string LibCardLim { get; set; }
+    }
+}
